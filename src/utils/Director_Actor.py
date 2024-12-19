cdfb7e20bc389a0bf944e9887af9304ca8a76f8e
Masca <alexandre.mascaro@epfl.ch>
2024-12-19 01:10:06 +0100

    Change Director

--- src/utils/Director_Actor.py
+++ src/utils/Director_Actor.py
@@ -13,7 +13,7 @@ import collections
 import scipy.stats
 
 # j'ai enlevé les bails à propos des realease date car je n'en avais pas besoin
-def create_actor_network_2(Actors, Movie, min_movies=50):
+def create_actor_network_V2(Actors, Movie, min_movies=50):
     """
     Create the actor network for actors that played in at least `min_movies` movies. 
     Each node is an actor with name, gender, ethnicity, and height as attributes.
@@ -60,6 +60,85 @@ def create_actor_network_2(Actors, Movie, min_movies=50):
     return G
 
 
+def create_Directed_bipartite_network_actor_director(Actors, Directors, min_movies=50):
+    # Calculate the number of films for each actor
+    Actors["Number_of_films"] = Actors["actor_age_atmovierelease"].apply(len)
+    
+    # Filter actors with at least `min_movies` films
+    actors_with_min_x_movies = Actors[Actors["Number_of_films"] >= min_movies]
+
+    # Explode the DataFrame to have one row per film per director
+    director_df = Directors.explode('Freebase_movie_ID')
+    
+    # Explode the DataFrame to have one row per film per actor
+    actors_df = actors_with_min_x_movies.explode('Freebase_movie_ID')
+
+    # Merge actors with movies to include directors
+    actors_with_directors = actors_df.merge(director_df[['Freebase_movie_ID', 'IMDb_director_ID']], on='Freebase_movie_ID', how='left')
+    
+    # Explode the directors list to have one row per director per actor
+    actors_with_directors = actors_with_directors.explode('IMDb_director_ID')
+
+    B = nx.DiGraph()
+
+    # Create nodes for actors
+    for _, actor in tqdm(actors_with_min_x_movies.iterrows(), desc="Nodes for actors"):
+        B.add_node(actor['Freebase_actor_ID'],bipartite = 0)
+
+    # Create nodes for directors
+    for _, director in tqdm(Directors.iterrows(), desc="Nodes for directors"):
+        B.add_node(director['IMDb_director_ID'],bipartite = 1)
+    
+    # Create edges
+    for _, actor in tqdm(actors_with_directors.iterrows(), desc ="Edges of network"):
+        actor_id = actor['Freebase_actor_ID']
+        director_id = actor['IMDb_director_ID']
+        # Skip rows without director information
+        if pd.isna(actor['IMDb_director_ID']): 
+            continue
+        # Checking if edge already exists
+        if B.has_edge(actor_id, director_id):
+            B[actor_id][director_id]['weight'] += 1
+        else:
+            B.add_edge(actor_id, director_id, weight=1)
+
+    # Remove nodes wich have no edges
+    B.remove_nodes_from(list(nx.isolates(B)))
+    
+    # Add attributes to each actor node
+    for _, actor in tqdm(actors_with_min_x_movies.iterrows(), desc='Adding actors attributes'):
+        actor_id = actor['Freebase_actor_ID']
+        if actor_id in B:
+            B.nodes[actor_id].update({
+                'name': actor['actor_name'],
+                'gender': actor.get('actor_gender', None),
+                'ethnicity': actor.get('ethnicity', None),
+                'height': actor.get('actor_height', None),
+            })
+        
+    # Add attributes to each director node
+    for _, director in tqdm(Directors.iterrows(), desc='Adding directors attributes'):
+        director_id = director['IMDb_director_ID']
+        if director_id in B:
+            B.nodes[director_id].update({
+               'name': director['director_name']
+            })
+
+    nodes_actor, nodes_director = nx.bipartite.sets(B)
+    for director in tqdm(nodes_director, desc="Computing weights for directors"):
+        Sum_Weight = 0
+        for actor in nodes_actor:
+            edge_data = B.get_edge_data(actor, director)
+            if edge_data:
+                Sum_Weight += edge_data["weight"]
+        if director in B:
+            B.nodes[director].update({
+               'weight': Sum_Weight
+            })
+    
+    return B
+
+
 def Actor_age_start(Actor):
     """
     Adds a column to the actor profile containing the starting age of their carreer
@@ -82,6 +161,20 @@ def Director_age_start(Director):
     
     Director["Career_Start_age"] = Director.apply(
     lambda row: min((val for val in row["age_at_movie_release"] if val > 0), default=np.nan),axis=1,)
+
+    return Director 
+
+
+def Actor_NOF(Actor):
+
+    Actor['Number_of_films'] = Actor['actor_age_atmovierelease'].apply(len)
+
+    return Actor 
+
+
+def Director_NOF(Director):
+
+    Director['Number_of_films'] = Director['age_at_movie_release'].apply(len)
 
     return Director 
 
@@ -149,7 +242,7 @@ def Number_per_genre(Movie, Min_movie=1000):
     # Computing the mean per genre
     exploded_movies = Movie_bis.explode('Movie_genres')
     Mean_per_genre = exploded_movies.groupby('Movie_genres')['Average rating'].mean().reset_index()
-    Mean_per_genre = Mean_per_genre.iloc[1:]
+    Mean_per_genre = Mean_per_genre.iloc[1:] # get rid of the nan
 
     # Merging both dataframe
     genre_counts = genre_counts.merge(Mean_per_genre[['Movie_genres','Average rating']], on='Movie_genres', how='left')
@@ -184,6 +277,16 @@ def Mean_movie_per_actor(Actor):
     return mean_movie_actor
 
 
+def Eigenvector_Mean_Median(Actor):
+    Mean = Actor['eigenvector_centrality_final'].mean()
+    Median = Actor['eigenvector_centrality_final'].median()
+
+    Actor_mean = Actor[Actor['eigenvector_centrality_final'] >= Mean]
+    Actor_median = Actor[Actor['eigenvector_centrality_final'] >= Median]
+
+    return Actor_mean, Actor_median
+
+
 def Splitting_Louvain(Graph,Actor):
     Graph_bis = Graph.copy()
     partition = community.best_partition(Graph_bis)
@@ -213,7 +316,7 @@ def Centrality_Louvain(Actor_cluster,Cluster,Movie):
         Actors_Groups[f'cluster_{cluster}'] = Actor_cluster[Actor_cluster['cluster_id'] == cluster].reset_index(drop=True)
 
     for cluster, actor in Actors_Groups.items():
-        played_together = create_actor_network_2(actor, Movie, 5)
+        played_together = create_actor_network_V2(actor, Movie, 5)
         nan_nodes = [node for node in played_together.nodes if pd.isna(node)]
         played_together.remove_nodes_from(nan_nodes)
         Networks[cluster] = played_together
@@ -237,15 +340,14 @@ def Top_centrality_Louvain(Actor_groups):
     return Actor_groups_top
 
 
-def keep_above_mean_rating(Actor,Actor_genre,Genre_counts):
+def keep_above_mean_rating(Actor,Genre_counts):
     # Defining empty list for the results 
     keep = []
-    keep_not = []
-    Actor_genre_bis = Actor_genre.copy().reset_index()
-    Actor_genre_bis = Actor_genre_bis.rename(columns = {'index':'Freebase_actor_ID'})
+    Actor_bis = Actor.copy()
+    Actor_bis = Actor_bis.set_index('Freebase_actor_ID',drop=False)
 
     # Checking for each actor if they are better than the average rating in their genre
-    for _,Act in tqdm(Actor_genre_bis.iterrows(), desc="Checking their relative ranking"):
+    for _,Act in tqdm(Actor_bis.iterrows(), desc="Checking their relative ranking"):
         Actor_id = Act['Freebase_actor_ID']
         Top_1 = Act['Top_1_Genre']
         Rating = Act['Top_1_Rating']
@@ -253,15 +355,28 @@ def keep_above_mean_rating(Actor,Actor_genre,Genre_counts):
             if Top_1 == Genre['Movie_genres'] and Rating >= Genre['Average rating']:
                 keep.append(Actor_id)
                 continue
-            if Top_1 == Genre['Movie_genres'] and Rating < Genre['Average rating']:    
-                keep_not.append(Actor_id)
-                continue
 
     # Keeping only the ones above the mean
-    Actor_new = Actor[Actor.index.isin(keep)]
-    Actor_genre_new = Actor_genre[Actor_genre.index.isin(keep)]
+    Actor_new = Actor_bis[Actor_bis.index.isin(keep)].reset_index(drop=True)
 
-    return Actor_new, Actor_genre_new, keep
+    return Actor_new, keep
+
+
+def keep_successful_actor(Actor, Genre_counts):
+    Actor_bis = Actor.set_index('Freebase_actor_ID',drop=False)
+    # Computing the criteria
+    Actor_12 = Actor_bis[Actor_bis['Number_of_films'] >= 12]
+    Actor_Rating,_ = keep_above_mean_rating(Actor_bis,Genre_counts)
+    Actor_Centrality,_ = Eigenvector_Mean_Median(Actor_bis)
+
+    # Applying the criteria
+    Actor_Success = Actor_12.copy()
+    Actor_Success = Actor_Success[Actor_Success['Freebase_actor_ID'].isin(Actor_Rating['Freebase_actor_ID'])]
+    Actor_Success = Actor_Success[Actor_Success['Freebase_actor_ID'].isin(Actor_Centrality['Freebase_actor_ID'])]
+
+    Actor_Success = Actor_Success.reset_index(drop=True)
+
+    return Actor_Success
 
 
 def Actor_genre_count_ratings(Actor, Movie, min_movies=5):
@@ -293,12 +408,14 @@ def Actor_genre_count_ratings(Actor, Movie, min_movies=5):
     genre_ratings = genre_ratings.apply(pd.to_numeric)
     
     # Computing the top genres per actor
-    Top_X_genre = genre_counts.apply(lambda row: list(row.sort_values(ascending=False).items()), axis=1)
+    Top_genre = genre_counts.apply(lambda row: list(row.sort_values(ascending=False).items()), axis=1)
+
+    # CA C'EST CHAT QUI M'A AIDéE PARCE QUE JE N'ARRIVAIS PAS à ARRANGER MON PROBLèME DE LIST EN UNE CELLE
     
     # Merging the results
     genre_ratings_counts = pd.DataFrame(index=genre_counts.index)
-    genre_ratings_counts[f'Top_1_Genre'] = Top_X_genre.apply(lambda x: f"{list(x)[1][0]}" if x != np.nan else None)
-    genre_ratings_counts[f'Top_1_Number'] = Top_X_genre.apply(lambda x: f"{list(x)[0][1]}" if x != np.nan else None)
+    genre_ratings_counts[f'Top_1_Genre'] = Top_genre.apply(lambda x: f"{list(x)[1][0]}" if x != np.nan else None)
+    genre_ratings_counts[f'Top_1_Number'] = Top_genre.apply(lambda x: f"{list(x)[0][1]}" if x != np.nan else None)
     genre_ratings_counts[f'Top_1_Rating'] = genre_ratings_counts.apply(
         lambda row: genre_ratings.loc[row.name, row[f'Top_1_Genre']]
         if pd.notna(row[f'Top_1_Genre']) and row[f'Top_1_Genre'] in genre_ratings.columns
@@ -308,46 +425,87 @@ def Actor_genre_count_ratings(Actor, Movie, min_movies=5):
     return genre_ratings_counts
 
 
-def Actor_genre_count_ratings_2(Actor, Movie, min_movies=5):
+def Director_genre_count_ratings(Director, Movie, min_movies=1):
     """
-    Compute the number of movies per genre and the average ratings of actors per genre.
-    Actors are considered only if they have played in at least `min_movies` movies.
+    Compute the number of movies per genre and the average ratings of directors per genre.
+    Directors are considered only if they have played in at least `min_movies` movies.
     """
     # Making a copy of the dataframes
-    Actor_bis = Actor.copy()
+    Director_bis = Director.copy()
     Movie_bis = Movie.copy()
 
     # Splitting the genre column into lists
     Movie_bis['Movie_genres'] = Movie_bis['Movie_genres'].str.split(', ')
     
     # Filter actors who have played in at least `min_movies` films
-    actors_with_min_x_movies = Actor_bis[Actor_bis['actor_age_atmovierelease'].apply(len) >= min_movies]
+    directors_with_min_x_movies = Director_bis[Director_bis['age_at_movie_release'].apply(len) >= min_movies]
     
     # Explode the actor's movie list into separate rows and filter by movie genre
-    df_explode = actors_with_min_x_movies.explode('Freebase_movie_ID')
+    df_explode = directors_with_min_x_movies.explode('Freebase_movie_ID')
     df_explode = df_explode.merge(Movie_bis[['Freebase_movie_ID', 'Movie_genres', 'Average rating']], on='Freebase_movie_ID', how='left')
     df_explode = df_explode.explode('Movie_genres')
 
     # Count the number of movie per genre
-    genre_counts = df_explode.groupby('Freebase_actor_ID')['Movie_genres'].value_counts().unstack(fill_value=0)
+    genre_counts = df_explode.groupby('IMDb_director_ID')['Movie_genres'].value_counts().unstack(fill_value=0)
     genre_counts = genre_counts.apply(pd.to_numeric)
 
     # Calculate the average rating per genre
-    genre_ratings = df_explode.groupby(['Freebase_actor_ID', 'Movie_genres'])['Average rating'].mean().unstack(fill_value=0)
+    genre_ratings = df_explode.groupby(['IMDb_director_ID', 'Movie_genres'])['Average rating'].mean().unstack(fill_value=0)
     genre_ratings = genre_ratings.apply(pd.to_numeric)
     
     # Computing the top genres per actor
-    Top_X_genre = genre_counts.apply(lambda row: list(row.sort_values(ascending=False).items()), axis=1)
+    Top_genre = genre_counts.apply(lambda row: list(row.sort_values(ascending=False).items()), axis=1)
+
+    # CA C'EST CHAT QUI M'A AIDéE PARCE QUE JE N'ARRIVAIS PAS à ARRANGER MON PROBLèME DE LIST EN UNE CELLE
     
     # Merging the results
     genre_ratings_counts = pd.DataFrame(index=genre_counts.index)
-    for i in range(5):
-        genre_ratings_counts[f'Top_{i+1}_Genre'] = Top_X_genre.apply(lambda x: f"{list(x)[i][0]}" if i < len(x) else None)
-        genre_ratings_counts[f'Top_{i+1}_Number'] = Top_X_genre.apply(lambda x: f"{list(x)[i][1]}" if i < len(x) else None)
-        genre_ratings_counts[f'Top_{i+1}_Rating'] = genre_ratings_counts.apply(
-        lambda row: genre_ratings.loc[row.name, row[f'Top_{i+1}_Genre']]
-        if pd.notna(row[f'Top_{i+1}_Genre']) and row[f'Top_{i+1}_Genre'] in genre_ratings.columns
+    genre_ratings_counts[f'Top_1_Genre'] = Top_genre.apply(lambda x: f"{list(x)[1][0]}" if x != np.nan else None)
+    genre_ratings_counts[f'Top_1_Number'] = Top_genre.apply(lambda x: f"{list(x)[0][1]}" if x != np.nan else None)
+    genre_ratings_counts[f'Top_1_Rating'] = genre_ratings_counts.apply(
+        lambda row: genre_ratings.loc[row.name, row[f'Top_1_Genre']]
+        if pd.notna(row[f'Top_1_Genre']) and row[f'Top_1_Genre'] in genre_ratings.columns
         else None,
         axis=1
     )
     return genre_ratings_counts
+
+
+def director_success1(Network, Director):
+    # Computes the actor and director part of network
+    nodes_actor, nodes_director = nx.bipartite.sets(Network)
+    
+    # Only keep the director part of the network
+    Director_Success = Director[Director['IMDb_director_ID'].isin(nodes_director)].copy()
+    Director_Success['Freebase_actor_ID'] = None  
+    Director_Success['Weight'] = None  
+    Director_Success['Sum_Weight'] = 0  
+    Director_Success["Number_of_actors"] = None
+
+    # Compute the weight, total weight and the actor wich worked under the director for each director
+    for director in tqdm(nodes_director, desc="Extracting attributes for directors"):
+        Actor_ID = []
+        Weight = []
+        Sum_Weight = 0
+        for actor in nodes_actor:
+            edge_data = Network.get_edge_data(actor, director)
+            if edge_data:
+                Actor_ID.append(actor)
+                Weight.append(edge_data["weight"])
+                Sum_Weight += edge_data["weight"]
+                #Actor_ID.append(actor)
+                #Weight.append(Network.get_edge_data(actor,director)["weight"])
+                #Sum_Weight += Network.get_edge_data(actor,director)["weight"]
+
+        # CA C'EST CHAT QUI M'A AIDéE PARCE QUE JE N'ARRIVAIS PAS à ARRANGER MON PROBLèME DE LIST EN UNE CELLE
+        
+        # Use .at to assign values to the cells as they are a list
+        idx = Director_Success[Director_Success['IMDb_director_ID'] == director].index.tolist()
+        idx = idx[0]
+        Director_Success.at[idx, 'Freebase_actor_ID'] = Actor_ID
+        Director_Success.at[idx, 'Weight'] = Weight
+        Director_Success.at[idx, 'Sum_Weight'] = Sum_Weight
+    
+    Director_Success["Number_of_actors"] = Director_Success["Freebase_actor_ID"].apply(len)
+
+    return Director_Success
